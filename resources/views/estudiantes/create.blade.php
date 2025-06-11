5464019d97484d811f05f235ee9cfc97bd3eb9b5
Omar Yova Aviles <yovalaviles812@gmail.com>
2025-06-10 18:48:02 -0600

    refactor: cuatrimestres establecidos

--- resources/views/estudiantes/create.blade.php
+++ resources/views/estudiantes/create.blade.php
@@ -106,8 +106,6 @@
                                                 <option value="6">6</option>
                                                 <option value="7">7</option>
                                                 <option value="8">8</option>
-                                                <option value="9">9</option>
-                                                <option value="10">10</option>
                                             </select>
                                             @error('cuatrimestre')
                                                 <div class="text-danger">{{ $message }}</div>
